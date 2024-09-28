8cd4c26ac9e367a3809660abb69c561c5c2ad0aa
alfaz785 <memonalfaz785@gmail.com>
2024-09-28 17:28:15 +0530

    change is done

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
 // import logo from './logo.svg';
-import './App.css';
-import Header from './MyComponents/Header';
-import Footer from './MyComponents/Footer';
-import { Content } from './MyComponents/Content';
-import AddForm from './MyComponents/AddForm';
-import React, { useState, useEffect } from 'react';
-import About from './MyComponents/About';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-
-} from "react-router-dom";
-import NoteState from './Context/note/NoteState';
-import Task from './MyComponents/Task';
-
+import "./App.css";
+import Header from "./MyComponents/Header";
+import Footer from "./MyComponents/Footer";
+import { Content } from "./MyComponents/Content";
+import AddForm from "./MyComponents/AddForm";
+import React, { useState, useEffect } from "react";
+import About from "./MyComponents/About";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NoteState from "./Context/note/NoteState";
+import Task from "./MyComponents/Task";
 
 function App() {
-
   let initData;
   if (localStorage.getItem("allData") === null) {
     initData = [];
-  }
-  else {
+  } else {
     initData = JSON.parse(localStorage.getItem("allData"));
   }
-
 
   // DELETE DATA --------------------------------------------------------
 
   let onDelete = (data) => {
     // console.log("I on Delete of content", data);
 
-    setCon(con.filter((e) => {
-      return e !== data;
-    }))
+    setCon(
+      con.filter((e) => {
+        return e !== data;
+      }),
+    );
     localStorage.setItem("allData", JSON.stringify(con));
-  }
-
+  };
 
   // ADD DATA ------------------------------------------------------------
 
@@ -47,61 +39,53 @@ function App() {
     let sno;
     if (con.length === 0) {
       sno = 1;
-    }
-    else {
+    } else {
       sno = con[con.length - 1].sno + 1;
-
     }
 
     const myForm = {
       sno: sno,
       title: title,
-      desc: 'Lorem ipsum dolor sit aMet consectetur adiPiSiCNng Elite: ' + desc,
-    }
+      desc: "Lorem ipsum dolor sit aMet consectetur adiPiSiCNng Elite: " + desc,
+    };
     // console.log(myForm);
     setCon([...con, myForm]);
+  };
 
-  }
-
-
-  // USE-STATE HOOK -------------------------------------------------------- 
+  // USE-STATE HOOK --------------------------------------------------------
 
   const [con, setCon] = useState(initData);
 
-
-  // USE-EFFECT HOOK -------------------------------------------------------- 
+  // USE-EFFECT HOOK --------------------------------------------------------
 
   useEffect(() => {
     localStorage.setItem("allData", JSON.stringify(con));
-
-  }, [con])
-
+  }, [con]);
 
   // APP-DATA RETURN-------------------------------------------------------
 
-  return (<>
-    <div>
-      <NoteState>
-        <Router>
-          <Header title="My App" SearchBar={false} />
+  return (
+    <>
+      <div>
+        <NoteState>
+          <Router>
+            <Header title="My App" SearchBar={false} />
 
-          <Routes>
+            <Routes>
+              <Route path="/" element={<AddForm addForm={addForm} />} />
 
-            <Route path='/' element={<AddForm addForm={addForm} />} />
+              <Route path="/about" element={<About />} />
 
-            <Route path='/about' element={<About />} />
+              <Route path="/task" element={<Task />} />
+            </Routes>
 
-            <Route path='/task' element = {<Task/>} />
+            <Content content={con} onDelete={onDelete} />
 
-          </Routes>
-
-          <Content content={con} onDelete={onDelete} />
-
-          <Footer />
-        </Router>
-      </NoteState>
-    </div >
-  </>
+            <Footer />
+          </Router>
+        </NoteState>
+      </div>
+    </>
   );
 }
 
